3e0664188a5ce98bc59671d95e39b4d173f942a6
Keydrain <clintgcooper@gmail.com>
2015-03-26 10:04:20 -0600

    Easy Challenge #3 Python
    Added support for uppercase letters. Program includes brute decrypt
    option.

--- Easy/Challenge #3/Python/code.py
+++ Easy/Challenge #3/Python/code.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
 def ceaser(char_in, offset):
-	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	loweralph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	upperalph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+	
 	try:
-		working = alphabet.index(char_in)
-		working += offset
-		if (working > len(alphabet)):
-			working = working-len(alphabet)
-		working = alphabet[working]
-		return working
+		if (char_in.istitle()):
+			working = upperalph.index(char_in)
+			working += offset
+			if (working > len(upperalph)):
+				working = working-len(upperalph)
+			return upperalph[working]
+		else:
+			working = loweralph.index(char_in)
+			working += offset
+			if (working > len(loweralph)):
+				working = working-len(loweralph)
+			return loweralph[working]
 	except:
 		return char_in
 
@@ -20,7 +28,15 @@ if (int(choice) == 1):
 		print(ceaser(char, int(shift)), end="")
 elif (int(choice) == 2):
 	sent = input("Please enter sentence to decrypt: ")
-	shift = input("Please enter offset to shift by: ")
-	for char in sent:
-		print(ceaser(char, -int(shift)), end="")
+	shift = input("Please enter offset to shift by. (~ will try all shifts): ")
+	if (shift == '~'):
+		for x in range(0,26):
+			string = str(x) + ": "
+			print("%4s" % string, end="")
+			for char in sent:
+				print(ceaser(char, -int(x)), end="")
+			print("")
+	else:
+		for char in sent:
+			print(ceaser(char, -int(shift)), end="")
 print("")
