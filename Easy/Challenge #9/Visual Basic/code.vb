20a8d41b8450ba737b9a0bbc6622f80ff10a4bfc
Keydrain <clintgcooper@gmail.com>
2015-04-04 09:05:17 -0600

    Easy Challenge #9 VB.NET
    Simple solution for Easy Challenge #9 in VB.NET. Feels like cheating,
    when using Array.Sort().

---
+++ Easy/Challenge #9/Visual Basic/code.vb
@@ -0,0 +1,32 @@
+'Solution to Easy Challenge #8
+'Author: Keydrain
+'Date: 04/04/15
+
+Imports System.IO
+
+Class code
+	
+	Public Shared Sub main()
+
+		Dim input As String()
+		Dim text As String
+
+		Console.WriteLine("")
+
+		Console.Write("Enter numbers and/or letters to be sorted: ")
+		text = Console.ReadLine()
+		input = text.Split()
+
+		Array.Sort(input)
+
+		Console.Write("Sorted:")
+
+		For Each item As String in input
+			Console.Write(" " & item)
+		Next
+
+		Console.WriteLine(VBCRLF)
+
+	End Sub
+
+End class
